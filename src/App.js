0888a41c32e0d781a491ca031e1b052aff192690
Jonathn bay <bayart.jonathan@laposte.net>
2023-02-25 11:45:51 +0100

    Memo-Hooks-React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
 
-function App() {
+const App = () => {
+
+  // 1. Les useState (état)
+  const [myNumber, setMyNumber] = useState(0);
+
+  // 2. Les useEffect(fonction fléchée, [callback])
+  useEffect(() => {
+    console.log("le nombre à changé");
+  }, [myNumber])
+
+  // 3. useRef
+  const selectedNumber = useRef();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <p>{myNumber}</p>
+
+      <button onClick={() => setMyNumber(Number(myNumber) + 1)}>incrémenter + 1</button>
+
+      <label htmlFor="">Numero de depart</label>
+      <input 
+      type="number"
+      defaultValue={0}
+      ref={selectedNumber}
+      onChange={() => setMyNumber(selectedNumber.current.value)}
+      />
+
     </div>
   );
-}
+};
 
 export default App;
